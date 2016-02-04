5e9560e5e0fdfd0cb0909f1cff67106964d9584d
sadman samee <Sadman.tonmoy@gmail.com>
2016-02-04 11:50:30 +0600

    Update App.java

--- MeteorAndroid/app/src/main/java/meteor/android/todo/App.java
+++ MeteorAndroid/app/src/main/java/meteor/android/todo/App.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import im.delight.android.ddp.Meteor;
 import im.delight.android.ddp.MeteorSingleton;
+import im.delight.android.ddp.ResultListener;
 
 /**
  * Created by Sadmansamee on 2/2/16.
@@ -11,7 +12,8 @@ import im.delight.android.ddp.MeteorSingleton;
 public class App extends Application {
 
     //This is your url set ws if not secured yet
-    String url = "wss://meteor-ios-todos.meteor.com/websocket";
+    String url2 = "wss://meteor-ios-todos.meteor.com/websocket";
+    String url = "wss://meteor-native-android-boilerplate.meteor.com/websocket";
 
     @Override
     public void onCreate() {
@@ -20,6 +22,7 @@ public class App extends Application {
 
         //we create a Singleton of Meteor instance and use it throughout the app life cycle
         MeteorSingleton.createInstance(this, url);
+
 
         //Enable when Debugging and better remove it when deploying
         Meteor.setLoggingEnabled(true);
